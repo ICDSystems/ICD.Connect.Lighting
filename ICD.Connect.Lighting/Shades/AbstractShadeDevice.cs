66696698d129da6bdb76af375defaa23ca2fd2e3
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-02-15 11:33:07 -0500

    Console commands for base class

--- ICD.Connect.Lighting/Shades/AbstractShadeDevice.cs
+++ ICD.Connect.Lighting/Shades/AbstractShadeDevice.cs
@@ -1,4 +1,6 @@
-﻿using ICD.Connect.Devices;
+﻿using System.Collections.Generic;
+using ICD.Connect.API.Commands;
+using ICD.Connect.Devices;
 
 namespace ICD.Connect.Lighting.Shades
 {
@@ -7,5 +9,32 @@ namespace ICD.Connect.Lighting.Shades
 	{
 		public abstract void Open();
 		public abstract void Close();
+
+
+		#region Console
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			yield return new ConsoleCommand("Open", "Opens the shade", () => Open());
+			yield return new ConsoleCommand("Close", "Closes the shade", ()=> Close());
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		#endregion
 	}
 }
