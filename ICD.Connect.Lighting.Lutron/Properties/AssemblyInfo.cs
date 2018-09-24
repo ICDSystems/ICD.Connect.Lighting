2ac2c2aa25302fc3a48ff49d3fe3e01f1df1dfa1
Chris Cameron <chris.cameron@wedoresi.com>
2018-09-24 12:06:00 -0400

    chore: Updating assembly info

--- ICD.Connect.Lighting.Lutron/Properties/AssemblyInfo.cs
+++ ICD.Connect.Lighting.Lutron/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Lighting.Lutron")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Lighting.Lutron")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("3.1.1.0")]
--- ICD.Connect.Lighting/Properties/AssemblyInfo.cs
+++ ICD.Connect.Lighting/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Lighting")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Lighting")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("3.1.1.0")]
 
