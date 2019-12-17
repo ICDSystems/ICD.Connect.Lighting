4b0c6555701b61731d5fd10d34d51050b8426c69
Drew Tingen <drew.tingen@icdsystems.com>
2019-12-17 18:01:33 -0500

    feat: Adding Relay Shade Device

---
+++ ICD.Connect.Lighting/Shades/RelayShadeDevice/RelayShadeDevice.cs
@@ -0,0 +1,110 @@
+﻿using ICD.Common.Utils.Timers;
+using ICD.Connect.Protocol.Ports.RelayPort;
+using ICD.Connect.Settings.Core;
+
+namespace ICD.Connect.Lighting.Shades.RelayShadeDevice
+{
+	public sealed class RelayShadeDevice : AbstractShadeDevice<RelayShadeDeviceSettings>
+	{
+		private IRelayPort m_OpenRelay;
+		private IRelayPort m_CloseRelay;
+		private long m_RelayCloseTime;
+
+		private readonly SafeTimer m_ResetOpenRelayTimer;
+		private readonly SafeTimer m_ResetCloseRelayTimer;
+
+		#region IShadeDevice
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsOnlineStatus()
+		{
+			return m_OpenRelay != null &&
+			       m_OpenRelay.IsOnline &&
+			       m_CloseRelay != null &&
+			       m_CloseRelay.IsOnline;
+		}
+
+		public override void Open()
+		{
+			if (m_OpenRelay == null)
+				return;
+
+			m_OpenRelay.Close();
+			m_ResetOpenRelayTimer.Reset(m_RelayCloseTime);
+
+		}
+
+		public override void Close()
+		{
+			if (m_CloseRelay == null)
+				return;
+
+			m_CloseRelay.Close();
+			m_ResetCloseRelayTimer.Reset(m_RelayCloseTime);
+		}
+
+		#endregion
+
+		public RelayShadeDevice()
+		{
+			m_ResetOpenRelayTimer = SafeTimer.Stopped(ResetOpenRelayTimerCallback);
+			m_ResetCloseRelayTimer = SafeTimer.Stopped(ResetCloseRelayTimerCallback);
+		}
+
+		private void ResetOpenRelayTimerCallback()
+		{
+			if (m_OpenRelay != null)
+				m_OpenRelay.Open();
+		}
+
+		private void ResetCloseRelayTimerCallback()
+		{
+			if (m_CloseRelay != null)
+				m_CloseRelay.Open();
+		}
+
+		#region Settings
+
+		/// <summary>
+		/// Override to apply settings to the instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="factory"></param>
+		protected override void ApplySettingsFinal(RelayShadeDeviceSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			m_OpenRelay = factory.GetOriginatorById<IRelayPort>(settings.OpenRelay);
+			m_CloseRelay = factory.GetOriginatorById<IRelayPort>(settings.CloseRelay);
+			m_RelayCloseTime = settings.RelayCloseTime;
+		}
+
+		/// <summary>
+		/// Override to apply properties to the settings instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		protected override void CopySettingsFinal(RelayShadeDeviceSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+			settings.OpenRelay = m_OpenRelay != null ? m_OpenRelay.Id : 0;
+			settings.CloseRelay = m_CloseRelay != null ? m_CloseRelay.Id : 0;
+			settings.RelayCloseTime = m_RelayCloseTime;
+		}
+
+		/// <summary>
+		/// Override to clear the instance settings.
+		/// </summary>
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			m_OpenRelay = null;
+			m_CloseRelay = null;
+			m_RelayCloseTime = 0;
+		}
+
+		#endregion
+	}
+}
---
+++ ICD.Connect.Lighting/Shades/RelayShadeDevice/RelayShadeDeviceSettings.cs
@@ -0,0 +1,43 @@
+﻿using ICD.Common.Utils.Xml;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Lighting.Shades.RelayShadeDevice
+{
+	[KrangSettings("RelayShadeDevice", typeof(RelayShadeDevice))]
+	public sealed class RelayShadeDeviceSettings : AbstractShadeDeviceSettings
+	{
+		private const string OPEN_RELAY_ELEMENT = "OpenRelay";
+		private const string CLOSE_RELAY_ELEMENT = "CloseRelay";
+		private const string RELAY_CLOSE_TIME_ELEMENT = "RelayCloseTime";
+
+		public int OpenRelay { get; set; }
+		public int CloseRelay { get; set; }
+		public long RelayCloseTime { get; set; }
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			OpenRelay = XmlUtils.ReadChildElementContentAsInt(xml, OPEN_RELAY_ELEMENT);
+			CloseRelay = XmlUtils.ReadChildElementContentAsInt(xml, CLOSE_RELAY_ELEMENT);
+			RelayCloseTime = XmlUtils.ReadChildElementContentAsLong(xml, RELAY_CLOSE_TIME_ELEMENT);
+		}
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(OPEN_RELAY_ELEMENT, IcdXmlConvert.ToString(OpenRelay));
+			writer.WriteElementString(CLOSE_RELAY_ELEMENT, IcdXmlConvert.ToString(CloseRelay));
+			writer.WriteElementString(RELAY_CLOSE_TIME_ELEMENT, IcdXmlConvert.ToString(RelayCloseTime));
+		}
+	}
+}
