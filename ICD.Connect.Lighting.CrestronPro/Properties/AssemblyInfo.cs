0b9cb98c23826d9d8f744ee985f023cd094d30e7
Chris Cameron <chris.cameron@wedoresi.com>
2020-01-03 10:28:29 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Lighting.CrestronPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Lighting.CrestronPro/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Lighting.CrestronPro")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Lighting.CrestronPro")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("4.1.0.0")]
--- ICD.Connect.Lighting/Properties/AssemblyInfo.cs
+++ ICD.Connect.Lighting/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Lighting")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Lighting")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("4.1.0.0")]
