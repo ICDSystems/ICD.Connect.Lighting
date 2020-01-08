654cf8482663a2ce1f048712a8b90eb10b1d3109
Drew Tingen <drew.tingen@icdsystems.com>
2020-01-08 15:59:18 -0500

    fix: Add KrangSettings attribute

--- ICD.Connect.Lighting.Lutron/Nwk/Devices/LutronNwk/LutronNwkDeviceSettings.cs
+++ ICD.Connect.Lighting.Lutron/Nwk/Devices/LutronNwk/LutronNwkDeviceSettings.cs
@@ -1,7 +1,9 @@
 ﻿using ICD.Connect.Lighting.Lutron.Nwk.Devices.AbstractLutronNwkDevice;
+using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Lighting.Lutron.Nwk.Devices.LutronNwk
 {
+	[KrangSettings("LutronNwk", typeof(LutronNwkDevice))]
 	public sealed class LutronNwkDeviceSettings : AbstractLutronNwkDeviceSettings
 	{
 	}
