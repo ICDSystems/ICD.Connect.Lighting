f9f9003110fae6fbbc9b6e4ebb6f454fcb8e5265
Drew Tingen <drew.tingen@icdsystems.com>
2020-04-06 18:05:16 -0400

    feat: MockShadeDevice implements IMockDevice

--- ICD.Connect.Lighting.Mock/MockShadeDevice.cs
+++ ICD.Connect.Lighting.Mock/MockShadeDevice.cs
@@ -1,20 +1,35 @@
-﻿using ICD.Common.Utils.Services.Logging;
+﻿using System.Collections.Generic;
+using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Devices.Mock;
 using ICD.Connect.Lighting.Shades;
 using ICD.Connect.Lighting.Shades.Controls;
+using ICD.Connect.Settings;
 
 namespace ICD.Connect.Lighting.Mock
 {
     public sealed class MockShadeDevice : AbstractShadeDevice<MockShadeDeviceSettings>,
-		IShadeWithStop
+		IShadeWithStop, IMockDevice
     {
+	    private bool m_IsOnline;
+
+	    public bool DefaultOffline { get; set; }
+
 	    public MockShadeDevice()
 	    {
 			Controls.Add(new ShadeStopControl<MockShadeDevice>(this, 1));
 		}
 
+	    public void SetIsOnlineState(bool isOnline)
+	    {
+		    m_IsOnline = isOnline;
+			UpdateCachedOnlineStatus();
+	    }
+
 	    protected override bool GetIsOnlineStatus()
 	    {
-		    return true;
+		    return m_IsOnline;
 	    }
 
 	    public override void Open()
@@ -31,5 +46,75 @@ namespace ICD.Connect.Lighting.Mock
 	    {
 			Logger.AddEntry(eSeverity.Debug, "Mock Shade {0} Stopped.", Id);
 		}
+
+	    #region Settings
+
+	    /// <summary>
+	    /// Override to apply settings to the instance.
+	    /// </summary>
+	    /// <param name="settings"></param>
+	    /// <param name="factory"></param>
+	    protected override void ApplySettingsFinal(MockShadeDeviceSettings settings, IDeviceFactory factory)
+	    {
+		    base.ApplySettingsFinal(settings, factory);
+
+			MockDeviceHelper.ApplySettings(this, settings);
+	    }
+
+	    /// <summary>
+	    /// Override to apply properties to the settings instance.
+	    /// </summary>
+	    /// <param name="settings"></param>
+	    protected override void CopySettingsFinal(MockShadeDeviceSettings settings)
+	    {
+		    base.CopySettingsFinal(settings);
+
+			MockDeviceHelper.CopySettings(this, settings);
+	    }
+
+	    /// <summary>
+	    /// Override to clear the instance settings.
+	    /// </summary>
+	    protected override void ClearSettingsFinal()
+	    {
+		    base.ClearSettingsFinal();
+
+			MockDeviceHelper.ClearSettings(this);
+	    }
+
+	    #endregion
+
+	    #region Console
+
+	    /// <summary>
+	    /// Gets the child console commands.
+	    /// </summary>
+	    /// <returns></returns>
+	    public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+	    {
+		    foreach (IConsoleCommand command in GetBaseConsoleCommands())
+			    yield return command;
+
+		    foreach (IConsoleCommand command in MockDeviceHelper.GetConsoleCommands(this))
+			    yield return command;
+	    }
+
+	    private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+	    {
+			return base.GetConsoleCommands();
+	    }
+
+	    /// <summary>
+	    /// Calls the delegate for each console status item.
+	    /// </summary>
+	    /// <param name="addRow"></param>
+	    public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+	    {
+		    base.BuildConsoleStatus(addRow);
+
+			MockDeviceHelper.BuildConsoleStatus(this, addRow);
+	    }
+
+	    #endregion
     }
 }
--- ICD.Connect.Lighting.Mock/MockShadeDeviceSettings.cs
+++ ICD.Connect.Lighting.Mock/MockShadeDeviceSettings.cs
@@ -1,10 +1,35 @@
-﻿using ICD.Connect.Lighting.Shades;
+﻿using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices.Mock;
+using ICD.Connect.Lighting.Shades;
 using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Lighting.Mock
 {
 	[KrangSettings("MockShade", typeof(MockShadeDevice))]
-	public sealed class MockShadeDeviceSettings : AbstractShadeDeviceSettings
+	public sealed class MockShadeDeviceSettings : AbstractShadeDeviceSettings, IMockDeviceSettings
 	{
+		public bool DefaultOffline { get; set; }
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			MockDeviceSettingsHelper.WriteElements(this, writer);
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			MockDeviceSettingsHelper.ParseXml(this, xml);
+		}
 	}
 }
