f69fdbb5f4c527b554f3553f19b7c2e6ef191b16
Chris Cameron <chris.cameron@wedoresi.com>
2020-05-05 10:43:28 -0400

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Lighting.Lutron/Properties/AssemblyInfo.cs
+++ ICD.Connect.Lighting.Lutron/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Lighting.Lutron")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("3.3.1.0")]
+[assembly: AssemblyVersion("3.4.0.0")]
