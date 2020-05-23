30a2703da93991c46450ad10a859ce3724148feb
Chris Cameron <chris.cameron@icdsystems.com>
2020-05-22 23:43:01 -0400

    refactor: Using AbstractSerialBuffer

--- ICD.Connect.Lighting.Lutron/Nwk/LutronQuantumNwkSerialBuffer.cs
+++ ICD.Connect.Lighting.Lutron/Nwk/LutronQuantumNwkSerialBuffer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
-using ICD.Common.Utils;
-using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Protocol.SerialBuffers;
 
@@ -11,27 +8,21 @@ namespace ICD.Connect.Lighting.Lutron.Nwk
 	/// <summary>
 	/// The LutronQuantumNwkSerialBuffer splits incoming data on a number of strings.
 	/// </summary>
-	public sealed class LutronQuantumNwkSerialBuffer : ISerialBuffer
+	public sealed class LutronQuantumNwkSerialBuffer : AbstractSerialBuffer
 	{
-		public event EventHandler<StringEventArgs> OnCompletedSerial;
-
-		private readonly StringBuilder m_RxData;
-		private readonly Queue<string> m_Queue;
-
-		private readonly SafeCriticalSection m_QueueSection;
-		private readonly SafeCriticalSection m_ParseSection;
-
-		private readonly string[] m_Delimiters =
+		private static readonly string[] s_Delimiters =
 		{
 			LutronUtils.CRLF,
 			LutronUtils.QNET
 		};
 
-		//These cause the buffer to immideately return, incluing the short circuit string
-		private readonly string[] m_ShortCircuits =
+		// These cause the buffer to immideately return, incluing the short circuit string
+		private static readonly string[] s_ShortCircuits =
 		{
 			LutronUtils.LOGIN_PROMPT
 		};
+
+		private readonly StringBuilder m_RxData;
 
 		/// <summary>
 		/// Constructor.
@@ -39,89 +30,47 @@ namespace ICD.Connect.Lighting.Lutron.Nwk
 		public LutronQuantumNwkSerialBuffer()
 		{
 			m_RxData = new StringBuilder();
-			m_Queue = new Queue<string>();
-
-			m_QueueSection = new SafeCriticalSection();
-			m_ParseSection = new SafeCriticalSection();
 		}
 
 		/// <summary>
-		/// Enqueues the serial data.
+		/// Override to clear any current state.
+		/// </summary>
+		protected override void ClearFinal()
+		{
+			m_RxData.Clear();
+		}
+
+		/// <summary>
+		/// Override to process the given item for chunking.
 		/// </summary>
 		/// <param name="data"></param>
-		public void Enqueue(string data)
+		protected override IEnumerable<string> Process(string data)
 		{
-			m_QueueSection.Execute(() => m_Queue.Enqueue(data));
-			Parse();
-		}
-
-		/// <summary>
-		/// Clears all queued data in the buffer.
-		/// </summary>
-		public void Clear()
-		{
-			m_ParseSection.Enter();
-			m_QueueSection.Enter();
-
-			try
+			while (true)
 			{
-				m_RxData.Clear();
-				m_Queue.Clear();
-			}
-			finally
-			{
-				m_ParseSection.Leave();
-				m_QueueSection.Leave();
-			}
-		}
+				string delimiter, shortCircuit;
+				int delimiterIndex = data.IndexOf(s_Delimiters, out delimiter);
+				int shortCircuitIndex = data.IndexOf(s_ShortCircuits, out shortCircuit);
 
-		/// <summary>
-		/// Searches the enqueued serial data for the delimiter character.
-		/// Complete strings are raised via the OnCompletedString event.
-		/// </summary>
-		private void Parse()
-		{
-			if (!m_ParseSection.TryEnter())
-				return;
-
-			try
-			{
-				string data = null;
-
-				while (m_QueueSection.Execute(() => m_Queue.Dequeue(out data)))
+				if (delimiterIndex < 0 && shortCircuitIndex < 0)
 				{
-					while (true)
-					{
-						string delimiter, shortCircuit;
-						int delimiterIndex = data.IndexOf(m_Delimiters, out delimiter);
-						int shortCircuitIndex = data.IndexOf(m_ShortCircuits, out shortCircuit);
-
-						if (delimiterIndex < 0 && shortCircuitIndex < 0)
-						{
-							m_RxData.Append(data);
-							break;
-						}
-
-						if (delimiterIndex >= 0)
-						{
-							m_RxData.Append(data.Substring(0, delimiterIndex));
-							data = data.Substring(delimiterIndex + delimiter.Length);
-						}
-						else
-						{
-							// Short Circuit includes the short circuit string, delimiter does not
-							m_RxData.Append(data.Substring(0, shortCircuitIndex + shortCircuit.Length));
-							data = data.Substring(shortCircuitIndex + shortCircuit.Length);
-						}
-
-						string output = m_RxData.Pop();
-						OnCompletedSerial.Raise(this, new StringEventArgs(output));
-					}
+					m_RxData.Append(data);
+					break;
 				}
-			}
-			finally
-			{
-				m_ParseSection.Leave();
+
+				if (delimiterIndex >= 0)
+				{
+					m_RxData.Append(data.Substring(0, delimiterIndex));
+					data = data.Substring(delimiterIndex + delimiter.Length);
+				}
+				else
+				{
+					// Short Circuit includes the short circuit string, delimiter does not
+					m_RxData.Append(data.Substring(0, shortCircuitIndex + shortCircuit.Length));
+					data = data.Substring(shortCircuitIndex + shortCircuit.Length);
+				}
+
+				yield return m_RxData.Pop();
 			}
 		}
 	}
