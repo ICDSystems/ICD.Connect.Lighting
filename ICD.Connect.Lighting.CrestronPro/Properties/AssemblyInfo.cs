e63ed247e0d3d6c729eae6f3c0191f7249cdca07
Drew Tingen <drew.tingen@icdsystems.com>
2023-03-22 08:18:34 -0400

    chore: Update copyright year

--- ICD.Connect.Lighting.CrestronPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Lighting.CrestronPro/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Lighting.CrestronPro")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Lighting.CrestronPro")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
 [assembly: AssemblyVersion("8.0.0.0")]
--- ICD.Connect.Lighting.Mock/Properties/AssemblyInfo.cs
+++ ICD.Connect.Lighting.Mock/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Lighting.Mock")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Lighting.Mock")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
 [assembly: AssemblyVersion("8.0.0.0")]
