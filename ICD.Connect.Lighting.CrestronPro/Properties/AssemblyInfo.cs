2f4c49665f86b01a13c0042e76512e7f4c364b30
Drew Tingen <drew.tingen@icdsystems.com>
2023-03-22 08:44:33 -0400

    chore: Update changelog, increment assembly patch version

--- ICD.Connect.Lighting.CrestronPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Lighting.CrestronPro/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Lighting.CrestronPro")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
-[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyVersion("8.0.1.0")]
